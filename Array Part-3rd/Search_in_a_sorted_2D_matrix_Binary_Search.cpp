4bc5b730716ce506cd999c8d7ec74e2af35c8e2e
Bhagyashri Tamrakar <86471670+BST82@users.noreply.github.com>
2022-11-07 15:46:04 +0530

    binary search method search in sorted array

--- Array Part-3rd/Search_in_a_sorted_2D_matrix_Binary_Search.cpp
+++ Array Part-3rd/Search_in_a_sorted_2D_matrix_Binary_Search.cpp
@@ -1,15 +1,24 @@
+
 #include<bits/stdc++.h>
 using namespace std;
 
 bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for (const auto &n: matrix) {
-            for (const auto &m: n) {
-                if (m == target) {
-                    return 1;
-                }
-                }
-            }
-            return 0;
+      int low=0;
+      if(!matrix.size()) return false;
+      int high=(matrix.size()*matrix[0].size())-1;
+
+      while (low<=high)
+      {
+        int mid=(low +(high+low)/2);
+        if(matrix[mid/matrix[0].size()][mid%matrix[0].size()]==target){
+            return true;
+        }
+        if(matrix[mid/matrix[0].size()][mid%matrix[0].size()]<target){
+            low=mid+1;
+        }
+        high=mid-1;
+      }
+      return false;
         }   
 int main(){
     int row;
@@ -48,5 +57,5 @@ cin>>target;
     return 0;
 }
 
-// TC : O(m*n);
+// TC : O(N*logN);
 //SC : O(1);
