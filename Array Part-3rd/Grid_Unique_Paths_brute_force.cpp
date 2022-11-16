5e5e69241d064a0511a172ebcbc4b20255f57f1e
Bhagyashri Tamrakar <86471670+BST82@users.noreply.github.com>
2022-11-16 22:30:40 +0530

    sde sheet

--- Array Part-3rd/Grid_Unique_Paths_brute_force.cpp
+++ Array Part-3rd/Grid_Unique_Paths_brute_force.cpp
@@ -2,19 +2,21 @@
 using namespace std;
 class Solution {
 public:
-     int countPaths(int i,int j,int n,int m)
-    {
-        if(i==(n-1)&&j==(m-1)) return 1;
-        if(i>=n||j>=m) return 0;
-        else return countPaths(i+1,j,n,m)+countPaths(i,j+1,n,m);
-    }
-    int uniquePaths(int m, int n) {
-       return countPaths(0,0,m,n);
-    }
+   int count_Paths(int i,int j,int n,int m){
+     if(i==(n-1)&&j==(m-1)) return 1;
+     if(i>n || j>m) return 0;
+     else  return count_Paths(i+1,j,n,m)+count_Paths(i,j+1,n,m);
+     }
+
+     int unique_path(int n,int m){
+        return count_Paths(0,0,n,m);
+     }
 };
 int main()
-{
+{      cout<<"Enter size of first array1 and array2 : ";
+    int n,m;
+    cin>>n>>m;
     Solution abc;
-    int totalCount= abc.uniquePaths(3,7);
+    int totalCount= abc.unique_path(n,m);
     cout<<"The total number of Unique Paths are "<<totalCount<<endl;
 }
